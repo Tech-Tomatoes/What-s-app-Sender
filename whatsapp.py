174053737831a9ba71775f97cc3321bce26c58f4
ShubhamGarg01 <72695387+ShubhamGarg01@users.noreply.github.com>
2021-05-20 13:26:21 +0530

    Update whatsapp.py
    What's app sender send the messages to all the contacts of the list which we want send the input message.

--- whatsapp.py
+++ whatsapp.py
@@ -1,9 +1,13 @@
 import pywhatkit
 import csv
 from datetime import datetime
-time = datetime.now()
-with open("Filepath.csv","r") as file:
+print("Enter the number")
+message = ["Hello","Welcome in Tech Tomatoes","Are u looking for intership"]
+for i in range(len(message)):
+        print(f"{i} - {message[i]}")
+msg = message[int(input())]        
+with open("FilePath.csv","r") as file:
     reader = csv.reader(file)
     for row in reader:
         time = datetime.now()
-        pywhatkit.sendwhatmsg(f"+91-{row[1]}","Welcome in Tech Tomatoes ",time.hour,time.minute+1,5)
+        pywhatkit.sendwhatmsg(f"+91-{row[1]}",msg,time.hour,time.minute+1,5)
