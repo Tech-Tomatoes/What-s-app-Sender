03fbea220ddaa22e2ac759a5b0038af40f8d5a5d
ShubhamGarg01 <72695387+ShubhamGarg01@users.noreply.github.com>
2021-06-15 13:49:10 +0530

    Update whatapp_update intergrate.py

--- whatapp_update intergrate.py
+++ whatapp_update intergrate.py
@@ -4,16 +4,14 @@ import pandas as pd
 import pywhatkit as kit
 from datetime import datetime
 
-if os.path.isfile("data.csv"):
-    df=pd.read_csv('data.csv')
+if os.path.isfile("C:data.csv"):
+    df=pd.read_csv('C:data.csv')
     #data=df[['number','ID']]
-    print(df.iterrows)
     time = datetime.now()
+    msg =input("Enter the message: ")
     for i,j in df.iterrows():
         time = datetime.now()
-        number_temp=j['number']
-        ID_temp=j['ID']
-        msg='Hello Customer,\n Your transation is successful and your transation ID is {}'.format(ID_temp)
+        number_temp=j['number']   
         if len(str(number_temp))==11:
             kit.sendwhatmsg(f"+91-{int(str(number_temp)[1:])}",msg,time.hour,time.minute+1,10)
         else:
